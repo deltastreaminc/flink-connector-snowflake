d6b414a839518913999c31b9e512279dd7c41c32
Mohsen Rezaei <7763122+mohsenrezaeithe@users.noreply.github.com>
2024-10-29 12:36:38 -0700

    Upgrade to Flink 1.19 + tooling cleanup (#40)
    * upgraded to Flink 1.19.1
    
    * moved to INFO for test logger
    
    * cleaned up tooling dependencies
    
    * removed incompatible annotation for Java 11

--- flink-connector-snowflake/src/test/java/io/deltastream/flink/connector/snowflake/sink/SnowflakeSinkITCase.java
+++ flink-connector-snowflake/src/test/java/io/deltastream/flink/connector/snowflake/sink/SnowflakeSinkITCase.java
@@ -6,14 +6,15 @@ import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.test.junit5.MiniClusterExtension;
 
+import org.apache.flink.shaded.guava31.com.google.common.collect.Maps;
+
 import io.deltastream.flink.connector.snowflake.sink.context.SnowflakeSinkContext;
 import io.deltastream.flink.connector.snowflake.sink.serialization.SnowflakeRowSerializationSchema;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.shaded.com.google.common.collect.Maps;
-import org.testcontainers.shaded.org.apache.commons.lang3.StringUtils;
-import org.testcontainers.shaded.org.apache.commons.lang3.SystemUtils;
 
 import java.util.Map;
 import java.util.UUID;
